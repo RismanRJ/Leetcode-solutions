fd7ee74b87a4fab2a2c3a1084c3602056c6c3f49
Risman J <rismanshanker21@gmail.com>
2024-08-03 12:55:51 +0530

    Time: 3 ms (100%), Space: 59.6 MB (35.28%) - LeetHub

---
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minSwaps(int[] nums) {
+        int totalOnes=0;
+        for(int i:nums)
+            totalOnes+=i;
+
+        if(totalOnes==nums.length || totalOnes==0)
+            return 0;
+        
+        int currOnes=0;
+        for(int i=0;i<totalOnes;i++)
+            currOnes+=nums[i];
+
+        int maxOnes=currOnes;
+
+        for(int i=0;i<nums.length;i++){
+            currOnes-=nums[i];
+            currOnes+= nums[(i+totalOnes)%nums.length];
+            maxOnes=Math.max(maxOnes,currOnes);
+        }
+
+        return totalOnes-maxOnes;
+
+    }
+}
