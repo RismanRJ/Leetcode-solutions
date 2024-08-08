36cbf4fcab48f10e59a593cf883fc476d0d739ec
Risman J <rismanshanker21@gmail.com>
2024-08-08 14:10:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        int cnt=1;
+        StringBuilder sb= new StringBuilder(a);
+
+        while(sb.length()<=b.length()){
+            sb.append(a);
+            cnt++;
+            if(sb.toString().contains(b))
+                return cnt;
+        }
+
+        return -1;
+        
+    }
+}
