a5ebf6b9e486bb5ca956f63eeb3fc2157112d062
Risman J <rismanshanker21@gmail.com>
2024-08-10 05:58:16 +0530

    Time: 0 ms (100%), Space: 41.6 MB (27.23%) - LeetHub

--- 0870-magic-squares-in-grid/0870-magic-squares-in-grid.java
+++ 0870-magic-squares-in-grid/0870-magic-squares-in-grid.java
@@ -2,61 +2,66 @@
 
 class Solution {
 
-    public static boolean isMagicSquare(int[][] grid, int rowStart, int colStart) {
-        HashSet<Integer> uniqueNumbers = new HashSet<>();
-        int sum = 0;
-        
-        // Check if the grid contains distinct numbers from 1 to 9
-        for (int i = rowStart; i < rowStart + 3; i++) {
-            for (int j = colStart; j < colStart + 3; j++) {
-                if (grid[i][j] < 1 || grid[i][j] > 9 || !uniqueNumbers.add(grid[i][j])) {
+    public static boolean isMagicSquare(int[][] grid, int row, int col){
+        boolean hash[] = new boolean[10];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                int num= grid[row+i][col+j];
+                if(num<1 || num>9) 
+                        return false;
+                if(hash[num])
                     return false;
-                }
+                hash[num]=true;
+                
             }
         }
+        //left diganol sum
+        int leftDiagSum=grid[row][col]+grid[row+1][col+1]+grid[row+2][col+2];
+
+        //right diganol sum
+        int rightDiagSum= grid[row][col+2]+grid[row+1][col+1]+grid[row+2][col];
+
+
+        if(leftDiagSum!=rightDiagSum)
+            return false;
         
-        // Calculate the sum of the first row
-        for (int j = colStart; j < colStart + 3; j++) {
-            sum += grid[rowStart][j];
+
+        //checking rows
+        int row1= grid[row][col]+ grid[row][col+1]+ grid[row][col+2];
+        int row2=  grid[row+1][col]+ grid[row+1][col+1]+ grid[row+1][col+2];
+        int row3=  grid[row+2][col]+ grid[row+2][col+1]+ grid[row+2][col+2];
+
+
+        //checking cols
+
+        int col1= grid[row][col]+grid[row+1][col]+grid[row+2][col];
+        int col2= grid[row][col+1]+grid[row+1][col+1]+grid[row+2][col+1];
+        int col3= grid[row][col+2]+grid[row+1][col+2]+grid[row+2][col+2];
+         if (!(row1 == leftDiagSum && row2 == leftDiagSum && row3 == leftDiagSum)) {
+            return false;
         }
         
-        // Check all rows
-        for (int i = rowStart + 1; i < rowStart + 3; i++) {
-            int rowSum = 0;
-            for (int j = colStart; j < colStart + 3; j++) {
-                rowSum += grid[i][j];
-            }
-            if (rowSum != sum) {
-                return false;
-            }
+         if (!(col1 == rightDiagSum && col2 == rightDiagSum && col3 == rightDiagSum)) {
+            return false;
         }
+
+        return true;
         
-        // Check all columns
-        for (int j = colStart; j < colStart + 3; j++) {
-            int colSum = 0;
-            for (int i = rowStart; i < rowStart + 3; i++) {
-                colSum += grid[i][j];
-            }
-            if (colSum != sum) {
-                return false;
-            }
-        }
-        
-        // Check diagonals
-        int diagSum1 = grid[rowStart][colStart] + grid[rowStart + 1][colStart + 1] + grid[rowStart + 2][colStart + 2];
-        int diagSum2 = grid[rowStart][colStart + 2] + grid[rowStart + 1][colStart + 1] + grid[rowStart + 2][colStart];
-        
-        return diagSum1 == sum && diagSum2 == sum;
+
+
     }
+
 
     public int numMagicSquaresInside(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         int magicSquares = 0;
-
+        if(n<3 || m<3){
+            return magicSquares;
+        }
         // Traverse each possible 3x3 subgrid
-        for (int i = 0; i <= n - 3; i++) {
-            for (int j = 0; j <= m - 3; j++) {
+        for (int i = 0; i+2 <n; i++) {
+            for (int j = 0;j+2<m; j++) {
                 if (isMagicSquare(grid, i, j)) {
                     magicSquares++;
                 }
