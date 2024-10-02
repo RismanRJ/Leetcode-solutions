52c1dc771e10b3320cd4341f5530d25ed61c8ce9
Risman J <rismanshanker21@gmail.com>
2024-10-02 19:03:03 +0530

    Time: 28 ms (55.21%), Space: 62.3 MB (45.54%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -1,19 +1,18 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
         int[] ans= new int[arr.length];
-        //to insert the all the elements in a sorted order
-        TreeMap<Integer,Integer>mpp= new TreeMap<>();
-        for(int i:arr)
-            mpp.put(i,mpp.getOrDefault(i,0)+1);
-        
+        int[] temp= arr.clone();
+        Arrays.sort(arr);
+        HashMap<Integer,Integer>mpp= new HashMap<>();
         int rank=1;
-        //assign rank to each element
-        for(Map.Entry<Integer,Integer>m:mpp.entrySet()){
-            mpp.put(m.getKey(),rank);
-            rank++;
+        for(int i:arr){
+            if(!mpp.containsKey(i)){
+                mpp.put(i,rank);
+                rank++;
+            }
         }
         for(int i=0;i<arr.length;i++){
-            ans[i]= mpp.get(arr[i]);
+            ans[i]= mpp.get(temp[i]);
         }
         return ans;
     }
