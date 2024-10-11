eab2fca16033d6927c8211ef0799af25451e556a
Risman J <rismanshanker21@gmail.com>
2024-10-11 22:24:56 +0530

    Time: 48 ms (63.16%), Space: 48.9 MB (64.33%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.java
@@ -12,12 +12,17 @@ class Solution {
         for(int i=0;i<times.length;i++){
             while(!trackingChairs.isEmpty() &&trackingChairs.peek()[0]<=times[i][0] )
                 availableChairs.offer(trackingChairs.poll()[1]);
+        //(when a person with less or equal leaving time than a incoming person
+        //there will be a seat available for him
+        //)
+
+        // offer him a new chair and remove the leaving person from the tracing Chairs
 
             if(times[i][0]==target)break;
 
             trackingChairs.offer(new int[]{times[i][1],availableChairs.poll()});
         }
 
-        return availableChairs.peek();
+        return availableChairs.peek(); // it will hold the targetFriend's chair number
     }
 }
