a8e627da6be42bbd0b56416982503c581f317f70
Risman J <rismanshanker21@gmail.com>
2024-10-13 19:48:04 +0530

    Time: 32 ms (64.23%), Space: 48.6 MB (65.85%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -14,21 +14,27 @@ class Solution {
 
         while(true){
             int[] arr= pq.poll();
-            int ele= arr[0];
+            int ele= arr[0]; // current minimum element in the heap
             int row=arr[1];
             int col= arr[2];
-
-            if(max_r-min_l>max_val-ele){
-                min_l=ele;
-                max_r=max_val;
+            //checking the current range based on max_right and min_left with curret maxVal in the heap and current min value in the heap
+            if(max_r-min_l>max_val-ele){ //(checking the range)){
+                min_l=ele; //updating the min_element with minimum element in the heap
+                max_r=max_val;//updating the max_element with maximum element in the heap
             }
-            if(col<nums.get(row).size()-1){
-                max_val= Math.max(max_val,nums.get(row).get(col+1));
+            if(col<nums.get(row).size()-1){ // check whether the col ranges out of bound
+            //it ensures that the range elements{l,r} should be within the range of elements in the list
+
+                max_val= Math.max(max_val,nums.get(row).get(col+1)); //updating the max element on every new adding element into the heap
                 pq.offer(new int[]{nums.get(row).get(col+1),row,col+1});
             }
             else break;
         }
 
         return new int[]{min_l,max_r};
+
+
+        // TC: o(nlogk)
+        //SC:o(k) [for heap storage]
     }
 }
