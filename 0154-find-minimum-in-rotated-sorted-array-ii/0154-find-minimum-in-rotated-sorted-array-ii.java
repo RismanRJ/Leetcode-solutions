b81a5733665cc70827f2c4856aab4fd6fe0f71a9
Risman J <rismanshanker21@gmail.com>
2024-12-05 22:35:40 +0530

    Time: 0 ms (100%), Space: 43.7 MB (6.02%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -6,23 +6,23 @@ class Solution {
         while(low<=high){
             int mid=(low+high)/2;
             
-            if(nums[low]==nums[mid] && nums[mid]== nums[high]){
-                low++;
-                high--;
-                ans= Math.min(ans,nums[mid]);
+            if(nums[low]==nums[mid] && nums[mid]== nums[high]){  // checking if the all elements are equal in rotated sorted array
+                low++; // shrink left portion in the array
+                high--; // shrink right portion in the array
+                ans= Math.min(ans,nums[mid]); // updating ans with minimum
                 continue;
             }
-            else if(nums[low]<=nums[mid] && nums[mid]<=nums[high]){
-                ans=Math.min(ans,nums[low]);
-                break;
+            else if(nums[low]<=nums[mid] && nums[mid]<=nums[high]){  // checking if the array is sorted in range(low--> high)
+                ans=Math.min(ans,nums[low]); // update the answer with nums[low]
+                break;  
             }
             else if(nums[low]<=nums[mid]){  // left half sorted array
                     ans= Math.min(ans,nums[low]);
-                    low=mid+1;
+                    low=mid+1; // eliminating left half
             }
             else{ // right half sorted array
                 ans=Math.min(ans,nums[mid]);
-                high=mid-1;
+                high=mid-1; // eliminating right half
             }
 
         }
