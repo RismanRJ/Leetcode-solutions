dae612e2a9870d71fe2c86c0ee944cd96e68fe88
Risman J <rismanshanker21@gmail.com>
2024-12-09 18:34:40 +0530

    Time: 26 ms (5.27%), Space: 45.5 MB (6.69%) - LeetHub

---
+++ 2331-intersection-of-multiple-arrays/2331-intersection-of-multiple-arrays.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> intersection(int[][] nums) {
+        int[] arr= new int[100001];
+        Arrays.fill(arr,0);
+        List<Integer>ans= new ArrayList<>();
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<nums[i].length;j++){
+               
+                arr[nums[i][j]]++;
+            }
+        }
+
+        for(int i=0;i<=arr.length-1;i++){
+                if(arr[i]==n)ans.add(i);
+        }
+        
+        return ans;
+    }
+}
