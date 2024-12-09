b38ba2df19d5f6125613624786a321cde190714a
Risman J <rismanshanker21@gmail.com>
2024-12-09 16:08:23 +0530

    Time: 5 ms (82.1%), Space: 49.6 MB (11.91%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
@@ -8,12 +8,12 @@ class Solution {
             curMax+=i;
             curMin+=i;
             total+=i;
-            maxSum=Math.max(maxSum,curMax);
-            minSum=Math.min(minSum,curMin);
+            maxSum=Math.max(maxSum,curMax); // kadane's algorithm to find max subarray sum
+            minSum=Math.min(minSum,curMin);// kadane's algorithm to find min subarray sum
             if(curMax<0)curMax=0;
             if(curMin>0)curMin=0;
         }
-
+        // check if all elements is negative then return maxSum, else Max(maxSum,(totalArraySum-minSum))
         return  maxSum<0?maxSum: Math.max(maxSum,total-minSum);
     
 
