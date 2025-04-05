d5d92e5c6331944e98cad96e215123d25cd69119
Risman J <rismanshanker21@gmail.com>
2025-04-05 22:20:08 +0530

    Time: 7 ms (68.23%), Space: 11.7 MB (68.58%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,11 +7,12 @@ public:
         for(int i=0,j=0;j<n;j++){
            
             if(mpp[s[j]]>0){
-                i= max(i,mpp[s[j]]);
+                i= max(i,mpp[s[j]]);  // starting from the next available position  of previously accessed character
             }
             int len = j-i+1;
             maxLen = max(len,maxLen);
-            mpp[s[j]]=j+1;
+            mpp[s[j]]=j+1;  // placing the acceses character indexed to its next position
+                            // for tracking the char if it comes again
         }
         return maxLen;
     }
