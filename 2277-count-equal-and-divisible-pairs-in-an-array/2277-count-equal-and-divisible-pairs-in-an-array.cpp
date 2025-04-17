1f1c43d57ab1db0ca856bc96789258511c885ed9
Risman J <rismanshanker21@gmail.com>
2025-04-17 08:37:29 +0530

    Time: 8 ms (3.85%), Space: 16.9 MB (6.09%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -5,9 +5,30 @@ public:
 
         int n = nums.size();
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==nums[j] && (i*j)%k==0) pairs++;
+        unordered_map<int,vector<int>> mpp;
+
+        for(int i=0;i<n;i++) mpp[nums[i]].push_back(i);  //precompute all the valid pairs with indices
+
+        vector<int> divisors;
+
+        for(int i=1;i*i<=k;i++){  // calculating the all valid divisors upto k - TC - sqrt(k)
+            if(k%i==0){
+                divisors.push_back(i);
+                if((k/i)!=i) divisors.push_back(k/i);
+            }
+        }
+
+
+        for(auto& [key,ind]: mpp){
+            unordered_map<int,int>tracker;
+            for(int i:ind){
+                int gcdd= gcd(i,k); // --> it will how many parts of k will be contributed in i
+                int need = k/gcdd; // --> nothing but j  -- remaining will be k/i
+                pairs+= tracker[need]; // how many pairs available before that index
+
+                for(int d: divisors){
+                    if(i%d==0) tracker[d]++; // check if the index is divisible by k
+                }
             }
         }
 
