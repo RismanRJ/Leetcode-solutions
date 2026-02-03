1e9018966340d0326f619618b4dec80ffac0d8a6
Risman J <rismanshanker21@gmail.com>
2026-02-03 22:22:02 +0530

    Time: 0 ms (100%), Space: 12.3 MB (40.11%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        int n1Idx = m-1;
-        int n2Idx= n-1;
-        int rt = m+n-1;
+       int i = m-1 , j = n-1;
+       int k  = m+n-1; // total len of array & idx to start the backfilling
 
-        while(n2Idx>=0){
-            if(n1Idx>=0 && nums1[n1Idx]>  nums2[n2Idx]){
-                nums1[rt]= nums1[n1Idx];
-                n1Idx--;
-            }
-            else {
-                nums1[rt] = nums2[n2Idx];
-                n2Idx--;
-            }
-            rt--;
-        }
+       while(j>=0){ // process still nums2 array len -> that is smaller one
+
+                if(i>=0 &&nums1[i]>nums2[j]){ // first array itself contains bigger one and check index out of bound when arr1 has all bigger elements than arr 2
+                        nums1[k--]=nums1[i--]; // update from last to avoid overwrites
+                }
+                else{
+                    nums1[k--]=nums2[j--];
+                }
+
+
+       }
         
     }
 };
