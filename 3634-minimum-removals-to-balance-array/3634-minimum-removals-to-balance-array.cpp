fb2eb8c0b4b53c89b57819e7bca8ca96a14dccb5
Risman J <rismanshanker21@gmail.com>
2026-02-06 11:55:47 +0530

    Time: 35 ms (48.84%), Space: 105.1 MB (14.24%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    #define ll long long 
+#define ll long long
     int minRemoval(vector<int>& nums, int k) {
-        
-        int minEle =INT_MAX, n = nums.size();
-        if(n==1) return 0;
-        sort(nums.begin(),nums.end());
-        int left =0,right=1;
-        while(right<n){
+
+        int minEle = INT_MAX, n = nums.size();
+        if (n == 1)
+            return 0;
+        sort(nums.begin(), nums.end());
+        int left = 0, right = 1;
+        while (right < n) {
             ll MIN = nums[left];
             ll MAX = nums[right];
-            if(MAX<=(k*MIN))minEle = min(minEle,n-(right-left+1));
-            else left++;
+            if (MAX <= (k * MIN))
+                minEle = min(minEle, n - (right - left + 1));
+            else
+                left++; // if condition is false , no need to check next ele,
+                        // however after that right idx also will be false for
+                        // this left indexed element , so move left pointer
             right++;
         }
 
-
-        return minEle ==INT_MAX ? n-1: minEle;
+        return minEle == INT_MAX
+                   ? n - 1
+                   : minEle; // If INT_MAX is ans , you need to remove the all
+                             // the elements except one , so n-1
     }
 };
